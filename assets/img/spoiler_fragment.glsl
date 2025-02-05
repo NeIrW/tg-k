aa43422d04e10441bb2e1eae36c0c4911cfaa6a3
NeIrW <NeIrW@live.com>
2025-02-05 10:27:49 +0800

    1
    1

--- assets/img/spoiler_fragment.glsl
+++ assets/img/spoiler_fragment.glsl
@@ -5,10 +5,19 @@ precision highp float;
 in float alpha;
 out vec4 fragColor;
 
+<<<<<<< Updated upstream
+=======
+uniform vec3 color;
+
+>>>>>>> Stashed changes
 void main() {
   vec2 c = 2.0 * gl_PointCoord - 1.0;
   if (dot(c, c) > 1.0) {
     discard;
   }
+<<<<<<< Updated upstream
   fragColor = vec4(1., 1., 1., alpha);
+=======
+  fragColor = vec4(color, alpha);
+>>>>>>> Stashed changes
 }
